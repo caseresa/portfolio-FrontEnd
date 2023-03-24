32a1a86147929268d97c457e747a18b41c573650
caseresa <anacaseres221@gmail.com>
2023-03-24 03:47:56 -0300

    primeros avances

--- portfolio-FrontEnd/src/app/app.module.ts
+++ portfolio-FrontEnd/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './layout/portfolio/portfolio.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { PrincipalComponent } from './layout/principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    HeaderComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
